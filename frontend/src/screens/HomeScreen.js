3812126343b4936e788e86eca5a0a124e4966a89
ScorchingShade <ankushors789@gmail.com>
2020-11-29 01:24:30 +0530

    added redux, dispatch, actions etc for homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,19 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import Product from '../components/Product'
-import axios from 'axios'
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 
 function HomeScreen() {
-  const [products, setProducts]=useState([]);
-  const [loading, setLoading]=useState(false);
-  const [error, setError]=useState(false);
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList)
+
+  const {loading,error,products}=productList;
 
   useEffect(()=>{
-    const fetchData=async()=>{
-      setLoading(true)
-      try{
-        const {data}= await axios.get('/api/v1/products');
-        setLoading(false)
-        setProducts(data);
-      } catch(err){
-        setError(err.message);
-        setLoading(false);
-      }
-      
-    }
-    fetchData()
+    dispatch(listProducts())
   },[])
 
     return (
