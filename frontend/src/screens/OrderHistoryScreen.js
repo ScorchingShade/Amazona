ee9c53b17f18f9df3bb92208d111760042c252a9
ScorchingShade <ankushors789@gmail.com>
2020-12-06 02:42:27 +0530

    Order history screen initialised

---
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function OrderHistoryScreen() {
+    return (
+        <div>
+            <h1>Order History</h1>
+            
+        </div> 
+    )
+}
+
+export default OrderHistoryScreen
