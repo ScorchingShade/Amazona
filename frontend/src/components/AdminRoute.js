fdb00cfd14270c301734beef5a7785308be90cb1
ScorchingShade <ankushors789@gmail.com>
2020-12-06 13:02:38 +0530

    added admin route completely

--- frontend/src/components/AdminRoute.js
+++ frontend/src/components/AdminRoute.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Redirect, Route } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route } from "react-router-dom";
 
-export default function PrivateRoute({ component: Component, ...rest }) {
+export default function AdminRoute({ component: Component, ...rest }) {
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   return (
     <Route
       {...rest}
       render={(props) =>
-        userInfo ? (
+        userInfo && userInfo.isAdmin ? (
           <Component {...props}></Component>
         ) : (
           <Redirect to="/signin" />
