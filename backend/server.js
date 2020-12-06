f21758c347565c925e58ce16568f107ae67c4b0b
ScorchingShade <ankushors789@gmail.com>
2020-12-07 02:05:03 +0530

    “publish”

--- backend/server.js
+++ backend/server.js
@@ -47,10 +47,10 @@ app.use((err, req, res, next) => {
 
 const __dirname = path.resolve();
 //Serving things from a particular folder
-app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/../frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 
 app.listen(PORT, () => {
