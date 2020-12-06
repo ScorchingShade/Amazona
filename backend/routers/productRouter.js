fb92a0441d613737848d0d677e1c7f3a0707eeaa
ScorchingShade <ankushors789@gmail.com>
2020-12-06 13:31:57 +0530

    created API for products for admin

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import data from "../data.js";
 import Product from "../models/productModel.js";
 import expressAsyncHandler from 'express-async-handler'
+import { isAdmin, isAuth } from "../utils.js";
 
 const productRouter = express.Router();
 
@@ -39,5 +40,27 @@ productRouter.get(
         res.status(200).send({ createdProducts });
     })
 );
+
+productRouter.post(
+    '/',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async(req, res) => {
+        const product = new Product({
+            name: 'samle name ' + Date.now(),
+            image: '/images/p1.jpg',
+            price: 0,
+            category: 'sample category',
+            brand: 'sample brand',
+            countInStock: 0,
+            rating: 0,
+            numReviews: 0,
+            description: 'sample description',
+        });
+        const createdProduct = await product.save();
+        res.send({ message: 'Product Created', product: createdProduct });
+    })
+);
+
 
 export default productRouter;
