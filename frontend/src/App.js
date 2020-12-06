aa24b8c4eb76513699a5d91d241a82cd6b499257
ScorchingShade <ankushors789@gmail.com>
2020-12-06 12:54:11 +0530

    added admin links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -78,6 +78,27 @@ function App() {
             ) : (
               <Link to="/signin">SignIn</Link>
             )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin<i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </header>
         <main>
