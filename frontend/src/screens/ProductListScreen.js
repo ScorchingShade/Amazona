f18bd8cea828247b40c8e01c4c1092bc03bf4a19
ScorchingShade <ankushors789@gmail.com>
2020-12-06 19:42:35 +0530

    added delete product functionality

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createProduct, listProducts } from "../actions/productActions";
+import {
+  createProduct,
+  deleteProduct,
+  listProducts,
+} from "../actions/productActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
-import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
+import { PRODUCT_CREATE_RESET, PRODUCT_DELETE_RESET } from "../constants/productConstants";
 
 function ProductListScreen(props) {
   const productList = useSelector((state) => state.productList);
@@ -17,16 +21,33 @@ function ProductListScreen(props) {
     product: createdProduct,
   } = productCreate;
 
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
+
+
   const dispatch = useDispatch();
   useEffect(() => {
-      if(successCreate){
-          dispatch({type:PRODUCT_CREATE_RESET});
-          props.history.push(`/product/${createdProduct._id}/edit`)
+    if (successCreate) {
+      dispatch({ type: PRODUCT_CREATE_RESET });
+      props.history.push(`/product/${createdProduct._id}/edit`);
+    }
+    if (successDelete) {
+        dispatch({ type: PRODUCT_DELETE_RESET });
       }
     dispatch(listProducts());
-  }, [createdProduct,dispatch,props.history,successCreate]);
-  const deleteHandler = () => {
+  }, [createdProduct, dispatch, props.history, successCreate,successDelete]);
+
+
+  const deleteHandler = (product) => {
     /// TODO: dispatch delete action
+    if (window.confirm('Are you sure to delete?')) {
+        dispatch(deleteProduct(product._id));
+      }
+    dispatch(deleteProduct(product._id));
   };
 
   const createHandler = () => {
@@ -42,8 +63,11 @@ function ProductListScreen(props) {
         </button>
       </div>
 
-        {loadingCreate && <LoadingBox></LoadingBox>}
-        {errorCreate && <MessageBox variant="danger">{errorCreate}</MessageBox>}
+      {loadingDelete && <LoadingBox></LoadingBox>}
+      {errorDelete && <MessageBox variant="danger">{errorDelete}</MessageBox>}
+
+      {loadingCreate && <LoadingBox></LoadingBox>}
+      {errorCreate && <MessageBox variant="danger">{errorCreate}</MessageBox>}
 
       {loading ? (
         <LoadingBox></LoadingBox>
