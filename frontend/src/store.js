1956b20a50316894838884c3649211a050fafe19
ScorchingShade <ankushors789@gmail.com>
2020-12-11 20:23:08 +0530

    added carousel for top sellers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -23,6 +23,7 @@ import {
     userListReducer,
     userRegisterReducer,
     userSigninReducer,
+    userTopSellerListReducer,
     userUpdateProfileReducer,
     userUpdateReducer,
 } from "./reducers/userReducer";
@@ -62,6 +63,7 @@ const reducer = combineReducers({
     orderDeliver: orderDeliverReducer,
     userList: userListReducer,
     userDelete: userDeleteReducer,
+    userTopSellersList: userTopSellerListReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
